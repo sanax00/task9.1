1c6d071c843ffe43e7cdc8bf7ecd3aa4433312a8
sanax00 <sana.mohamed3333@gmail.com>
2021-08-19 16:16:05 +0200

    task9

---
+++ lib/main.dart
@@ -0,0 +1,138 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  initState(){
+
+    super.initState();
+  }
+SaveData(String a) async{
+var directory = await getApplicationDocumentsDirectory();
+var mypath =directory.path;
+var file = File("$mypath/data.txt");
+file.writeAsString(a);
+
+}
+GetData() async{
+  var directory = await getApplicationDocumentsDirectory();
+  var mypath =directory.path;
+  var file = File("$mypath/data.txt");
+  var s= file.readAsString();
+  return s;
+}
+
+  TextEditingController myname = new TextEditingController();
+  String? s;
+  @override
+  Widget build(BuildContext context) {
+
+
+
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+       child: Column(
+         children: [
+           Container(
+             child: TextFormField(
+               controller: myname,
+               decoration: InputDecoration(
+                   border: UnderlineInputBorder(),
+                   labelText: 'Enter your Name'
+               ),
+             ),
+           ),
+           SizedBox(height: 10),
+           Container(
+             child: FlatButton(
+             child: Text('Save Data', style: TextStyle(fontSize: 20.0),),
+             color: Colors.blueAccent,
+             textColor: Colors.white,
+             onPressed: () {
+               SaveData(myname.text);
+               setState(() {
+
+               });
+             },
+           ),
+
+           ),
+           SizedBox(height: 10),
+           Container(
+             child: FlatButton(
+               child: Text('Read Data', style: TextStyle(fontSize: 20.0),),
+               color: Colors.blueAccent,
+               textColor: Colors.white,
+               onPressed: ()
+               async
+               {
+
+                 s=await GetData();
+                 setState(() {
+
+                 });
+               },
+             ),
+
+           ),
+           SizedBox(height: 10),
+           Container(
+              child: Text(
+    "Hello, ${s??"noname"}",
+    textAlign: TextAlign.center,
+    overflow: TextOverflow.ellipsis,
+    style: const TextStyle(fontWeight: FontWeight.bold),
+    )
+
+           ),
+
+         ],
+       )
+
+
+
+
+
+
+        ),
+
+
+    );
+  }
+}
